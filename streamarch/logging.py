67b26f3c0791209397875bb7b53783aa886e4a3d
Julian Mulla <jmulla@gmail.com>
2024-06-16 03:05:24 +0000

    fix path

--- streamarch/logging.py
+++ streamarch/logging.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 def setup_logging(target):
+
     fn = '5-queued-stderr-json-file'
-    config_file = Path(f"logging_configs/{fn}.json")
+    config_file = Path(__file__).parent / "logging_configs" / f"{fn}.json"
 
     with open(config_file) as f_in:
         config = json.load(f_in)
